9d05cb8270b083faa7b9917849d27a624be68243
sowmik-sec <sowmik.sec@gmail.com>
2024-01-08 12:41:11 +0600

    create your own react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,10 @@
 import Chai from "./Chai";
 function App() {
+  const username = "chai and code";
   return (
     <>
       <Chai />
+      <h1>chai aur react {username}</h1>
     </>
   );
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,4 +1,38 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import App from "./App";
+import { jsx as _jsx } from "react/jsx-runtime";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!!!!!!!!</h1>
+    </div>
+  );
+}
+
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+const anotherUser = "habibi";
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit google
+  </a>
+);
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit googleeee",
+  anotherUser
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
