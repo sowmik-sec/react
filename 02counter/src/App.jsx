88510d66b7b37ea911ee97868655ed4ff73d8856
sowmik-sec <sowmik.sec@gmail.com>
2024-01-09 21:52:54 +0600

    useState hook in react

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(5);
+  const addValue = () => {
+    if (counter === 20) {
+      alert(`You can't add value more than 20`);
+      return;
+    }
+    setCounter(counter + 1);
+  };
+  const removeValue = () => {
+    if (counter === 0) {
+      alert(`You can't decrease value less than 0`);
+      return;
+    }
+    setCounter(counter - 1);
+  };
   return (
     <>
       <h1>Hello world</h1>
-      <h2>Counter value: 5</h2>
-      <button>Add value</button>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>
       <br />
-      <button>Remove value</button>
+      <button onClick={removeValue}>Remove value</button>
     </>
   );
 }
