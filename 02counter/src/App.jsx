626870d03b337ad4db2bc035a42dc910b346dc3a
sowmik-sec <sowmik.sec@gmail.com>
2024-01-09 21:21:29 +0600

    new project initialized

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <h2>Counter value: 5</h2>
+      <button>Add value</button>
+      <br />
+      <button>Remove value</button>
+    </>
+  );
+}
+
+export default App;
