67be6ed89d82815295e02cde94c5c1dc3c200739
sowmik-sec <sowmik.sec@gmail.com>
2024-01-10 12:21:55 +0600

    tailwind and props in reactjs

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
+  // let myObj = {
+  //   username: "sowmik",
+  //   age: 28,
+  // };
+  // let newArr = [1, 2, 3, 4];
   return (
     <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind test</h1>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind test
+      </h1>
+      <Card username="sowmik" btnText="click me" />
+      <Card username="habib" />
     </>
   );
 }
