f02e02f06c871a50f0e24b8f6cd9acfe6ad53604
sowmik-sec <sowmik.sec@gmail.com>
2024-01-12 10:59:53 +0600

    create app ui

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -7,9 +7,15 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="p-4 bg-pink-400 text-3xl text-white">hello world</h1>
-    </>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* themeBtn */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">{/* Card  */}</div>
+      </div>
+    </div>
   );
 }
 
