92085d5ab573a6544708658ca9267a5c629f16bf
sowmik-sec <sowmik.sec@gmail.com>
2024-01-13 21:37:20 +0600

    add edit and toggle functionality

--- 10todocontextLocal/src/components/TodoItem.jsx
+++ 10todocontextLocal/src/components/TodoItem.jsx
@@ -2,8 +2,17 @@ import { useTodo } from "../contexts";
 
 function TodoItem({ todo }) {
   const [isTodoEditable, setIsTodoEditable] = useState(false);
-  const [todoMessage, setTodoMessage] = useState(todo.todo);
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
   const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsTodoEditable(false);
+  };
+
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
 
   return (
     <div
