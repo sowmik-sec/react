62958bbce148bd1862b455c914c86b3e2f52dfa6
sowmik-sec <sowmik.sec@gmail.com>
2024-01-14 11:46:54 +0600

    create app ui

--- 09themeswitcher-recap/src/App.jsx
+++ 09themeswitcher-recap/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="p-4 bg-pink-400 text-3xl">Hello World</h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themeBtn */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+        </div>
+      </div>
     </>
   );
 }
