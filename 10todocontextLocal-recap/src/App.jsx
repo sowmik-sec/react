985870b0bb7abef8aa6eb39dee9937b7eff0b22d
sowmik-sec <sowmik.sec@gmail.com>
2024-01-14 20:34:32 +0600

    create crud functionality to todo project

--- 10todocontextLocal-recap/src/App.jsx
+++ 10todocontextLocal-recap/src/App.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
 import "./App.css";
+import { TodoProvider } from "./contexts";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id ? { ...prevTodo, completed: !completed } : prevTodo
+      )
+    );
+  };
+
   return (
-    <>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
@@ -14,7 +42,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </TodoProvider>
   );
 }
 
