1a97e3b3c6e5fa828f944c90040943e93f5db066
sowmik-sec <sowmik.sec@gmail.com>
2024-01-14 12:06:42 +0600

    add theme changing functionality

--- 09themeswitcher-recap/src/App.jsx
+++ 09themeswitcher-recap/src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual change in theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <h1 className="p-4 bg-pink-400 text-3xl">Hello World</h1>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
@@ -13,7 +31,7 @@ function App() {
           <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
         </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
