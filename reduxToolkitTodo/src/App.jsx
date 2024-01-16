c8b035044854619e3653b30e8c54b0fcd05b4263
sowmik-sec <sowmik.sec@gmail.com>
2024-01-16 12:30:17 +0600

    show components in ui

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
       <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
