a2cab958aff4f69f152373ef7036f44e251e133d
sowmik-sec <sowmik.sec@gmail.com>
2024-01-18 11:16:13 +0600

    redux store

--- REDUX-Anisul/index.js
+++ REDUX-Anisul/index.js
@@ -1,3 +1,5 @@
+const { createStore } = require("redux");
+
 //define constants
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
@@ -47,4 +49,13 @@ const counterReducer = (state = initialCounterState, action) => {
 //1. state
 //2. dispatch action
 //3. reducer
-//4. store
+//4. store - getState(), dispatch(), subscribe()
+
+//create store
+const store = createStore(counterReducer);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// dispatch action
+store.dispatch(incrementCounter());
