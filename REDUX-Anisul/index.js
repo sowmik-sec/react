dc05e3de3f957df8158b7a81958f3e4b708fd93a
sowmik-sec <sowmik.sec@gmail.com>
2024-01-18 11:00:51 +0600

    redux reducers

--- REDUX-Anisul/index.js
+++ REDUX-Anisul/index.js
@@ -23,11 +23,25 @@ const decrementCounter = () => {
     type: DECREMENT,
   };
 };
-const addUser = () => {
-  return {
-    type: ADD_USER,
-    payload: { name: "shakil" },
-  };
+
+// create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    default:
+      break;
+  }
 };
 
 //1. state
