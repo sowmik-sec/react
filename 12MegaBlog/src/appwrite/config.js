2e1228a3b15764606173f57a7acd6d052cbda4df
sowmik-sec <sowmik.sec@gmail.com>
2024-01-21 12:44:31 +0600

    create get all post functionality

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -74,6 +74,18 @@ export class Service {
       return false;
     }
   }
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPosts :: error ", error);
+      return false;
+    }
+  }
 }
 
 const service = new Service();
