3c37a46e1ce9e39e8b3100a1a15129052240359d
sowmik-sec <sowmik.sec@gmail.com>
2024-01-21 12:41:52 +0600

    make create post functionality

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -14,6 +14,24 @@ export class Service {
     this.databases = new Databases(this.client);
     this.bucket = new Storage(this.client);
   }
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite service :: error ", error);
+    }
+  }
 }
 
 const service = new Service();
