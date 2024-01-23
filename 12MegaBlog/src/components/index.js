eb59c9c04c459d80159b9649caea3f0982a55994
sowmik-sec <sowmik.sec@gmail.com>
2024-01-23 20:58:43 +0600

    import and export RTE

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,4 +3,5 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
-export { Header, Footer, Container, Logo, LogoutBtn };
+import RTE from "./RTE";
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
